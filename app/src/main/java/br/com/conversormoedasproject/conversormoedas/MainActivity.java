45d757201a1b4323096066d2583017e33679de4b
Islan Rodrigues <jose-islan52@hotmail.com>
2018-08-09 15:01:49 -0300

    button action implementation

--- app/src/main/java/br/com/conversormoedasproject/conversormoedas/MainActivity.java
+++ app/src/main/java/br/com/conversormoedasproject/conversormoedas/MainActivity.java
@@ -2,11 +2,12 @@ package br.com.conversormoedasproject.conversormoedas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -20,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDolar = (TextView) findViewById(R.id.text_dolar);
         this.mViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
-        this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
 
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.button_calculate) {
+            // lógica do botão aqui
+        }
+
+    }
 
 
     private static class ViewHolder {
